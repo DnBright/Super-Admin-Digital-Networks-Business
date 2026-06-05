dd06b828f64a50f4dc3691a37c364ce63ca247ab
DnBright <dnbright000@gmail.com>
2026-06-06 02:56:12 +0700

    “update”

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Root landing page
+// Root landing page redirects to /global-command
 Route::get('/', function () {
-    return view('index');
+    return redirect('/global-command');
+});
+
+// Master Control Pages
+Route::get('/global-command', function () {
+    return view('index', ['tab' => 'global_command']);
+});
+
+Route::get('/access-control', function () {
+    return view('index', ['tab' => 'access_control']);
+});
+
+Route::get('/system-settings', function () {
+    return view('index', ['tab' => 'system_settings']);
 });
 
 // Load sub-route files from the web/ directory
